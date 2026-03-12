a1bf159a07c3a6d3fd86fa7f1cc7462a16f784ad
Danny-luvantSIO <danny.luvant@ldu.nc>
2026-03-12 14:44:22 +1100

    Update tp6ex7.py

--- tp6ex7.py
+++ tp6ex7.py
@@ -1,7 +1,8 @@
-input_age = int(input("Entrez votre âge : "))   
 def verifier_majorite(age):
     if age >= 18:
         print("majeur")
     else:
         print("mineur")
-verifier_majorite(input_age)
+verifier_majorite(20)
+verifier_majorite(15)
+verifier_majorite(30)
